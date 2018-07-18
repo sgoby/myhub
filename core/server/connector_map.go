d77e5e9ec5e66b50feb86d69225ac820ad2a24a2
sangzh <sangzh@34era.com>
2018-07-18 17:10:01 +0800

    update
    update

--- core/server/connector_map.go
+++ core/server/connector_map.go
@@ -21,7 +21,7 @@ import (
 )
 
 type ConnectorMap struct {
-	table []*hubclient.Connector
+	table map[int64]*hubclient.Connector
 	lock  chan int
 }
 
@@ -29,6 +29,7 @@ type ConnectorMap struct {
 func NewConnectorMap() *ConnectorMap {
 	return &ConnectorMap{
 		lock: make(chan int, 1),
+		table:make(map[int64]*hubclient.Connector),
 	}
 }
 func (cm *ConnectorMap) Len() int {
@@ -37,24 +38,22 @@ func (cm *ConnectorMap) Len() int {
 func (cm *ConnectorMap) Put(c *hubclient.Connector) {
 	cm.lock <- 1
 	defer func() { <- cm.lock }()
-	cm.table = append(cm.table, c)
+	if c == nil{
+		return
+	}
+	cm.table[c.GetConnectionID()] = c
 }
 func (cm *ConnectorMap) Get(id int64) (c *hubclient.Connector, ok bool) {
 	cm.lock <- 1
 	defer func() { <- cm.lock }()
-	c, _ = cm.find(id)
-	if c != nil {
-		return c, true
-	}
-	return nil, false
-
+	c,ok = cm.table[id]
+	return
 }
 func (cm *ConnectorMap) GetSlice() (s []*hubclient.Connector) {
 	cm.lock <- 1
 	defer func() { <- cm.lock }()
-	s = make([]*hubclient.Connector,len(cm.table))
-	for i,c := range cm.table{
-		s[i] = c
+	for _,c := range cm.table{
+		s = append(s,c)
 	}
 	return s
 }
@@ -62,20 +61,9 @@ func (cm *ConnectorMap) GetSlice() (s []*hubclient.Connector) {
 func (cm *ConnectorMap) Del(id int64) (c *hubclient.Connector) {
 	cm.lock <- 1
 	defer func() { <- cm.lock }()
-	c, index := cm.find(id)
-	if c == nil {
-		return nil
+	cc,ok := cm.table[id]
+	if ok{
+		delete(cm.table,id)
 	}
-	len := len(cm.table)
-	copy(cm.table[index:], cm.table[index+1:])
-	cm.table = cm.table[:len-1]
-	return c
-}
-func (cm *ConnectorMap) find(id int64) (c *hubclient.Connector, index int) {
-	for i, c := range cm.table {
-		if c.GetConnectionID() == id {
-			return c, i
-		}
-	}
-	return nil, 0
+	return cc
 }
